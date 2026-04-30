3ebafdabd44d1cf86df24aea07e62aa4ee05aa04
micha3lny <72762390+micha3lny@users.noreply.github.com>
2026-04-30 17:42:54 +0200

    add v27 entry-first config

---
+++ src/strategies/momentum_trailing_intraday/configs_v27.py
@@ -0,0 +1,22 @@
+V27_ENTRY_CORE = {
+    "market_regime_filter": False,
+    "min_avg_daily_range": 4.0,
+
+    "daily_trend_min": -30.0,
+    "daily_trend_max": -3.0,
+
+    "min_breakout_pct": 0.8,
+    "max_breakout_pct": 1.8,
+
+    "min_15m_confirmation_close_strength": 0.20,
+    "max_15m_confirmation_close_strength": 1.0,
+
+    "min_pullback_pct": 0.20,
+    "max_pullback_pct": 3.50,
+    "max_5m_close_strength": 0.85,
+
+    "min_1m_close_strength": 0.75,
+    "max_1m_close_strength": 0.90,
+
+    "max_entry_risk_pct": 10.0,
+}
